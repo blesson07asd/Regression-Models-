41b5d1fd81b8e3b4b058437c002050ccb2ff5f8f
Blesson <blessonthomas2005@gmail.com>
2025-05-06 18:48:27 +0530

    Linear Regression

---
+++ housepricepre.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score 
+
+data=pd.read_csv("houseprice.csv")
+data=pd.DataFrame(data)
+data = pd.get_dummies(data) 
+data.dropna(inplace=True)
+
+model=LinearRegression()
+x=data.drop(columns=["median_house_value"])
+y=data["median_house_value"]
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2,random_state=42)        
+model.fit(x_train,y_train)
+y_pred=model.predict(x_test)    
+
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
+print("R^2 Score:", r2_score(y_test, y_pred))   
