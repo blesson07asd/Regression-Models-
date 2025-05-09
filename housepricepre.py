92514aeae474a4f2dfe5eb3e7760c949b8a560da
Blesson <blessonthomas2005@gmail.com>
2025-05-09 11:10:36 +0530

    ..

--- housepricepre.py
+++ housepricepre.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score 
+#from sklearn.metrics import mean_squared_error, r2_score 
 
 data=pd.read_csv("houseprice.csv")
 data=pd.DataFrame(data)
